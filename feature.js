66757f4707634b1801df39fd28f82006e274061f
Phinehas Beresford <78656905+Cracko298@users.noreply.github.com>
2024-01-25 11:09:41 -0500

    Create feature.js

---
+++ feature.js
@@ -0,0 +1,9 @@
+// Disable right-click
+document.addEventListener('contextmenu', function (e) {
+    e.preventDefault();
+});
+
+// Disable text selection
+document.addEventListener('selectstart', function (e) {
+    e.preventDefault();
+});
